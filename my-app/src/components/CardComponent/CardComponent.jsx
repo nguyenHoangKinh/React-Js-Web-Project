ff67f1ddafcc464d66c28fca4d69224561daedc1
NguyenHoangKinh29808 <21211tt4678@mail.tdc.edu.vn>
2023-11-18 04:29:34 +0700

    cap nhat lai card Product

---
+++ my-app/src/components/CardComponent/CardComponent.jsx
@@ -0,0 +1,57 @@
+import React from "react";
+import {
+  StyleNameProduct,
+  WrapperCardStyle,
+  WrapperDiscountText,
+  WrapperPriceText,
+  WrapperReportText,
+  WrapperStyleTextSell,
+} from "./style";
+import { StarFilled } from "@ant-design/icons";
+import logo from "../../assets/images/logo.png";
+
+const CardComponent = () => {
+  return (
+    <WrapperCardStyle
+      hoverable
+      headStyle={{ width: "200px", height: "200px" }}
+      style={{ width: 200 }}
+      bodyStyle={{ padding: "10px" }}
+      cover={
+        <img
+          alt="example"
+          src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png"
+        />
+      }
+    >
+      <img
+        src={logo}
+        style={{
+          width: "68px",
+          height: "14px",
+          position: "absolute",
+          top: -1,
+          left: -1,
+          borderTopLeftRadius: "3px",
+        }}
+      />
+      <StyleNameProduct>Iphome</StyleNameProduct>
+      <WrapperReportText>
+        <span style={{ marginRight: "4px" }}>
+          <span>65.34</span>
+          <StarFilled
+            style={{ fontSize: "12px", color: "rgb(253, 216, 54)" }}
+          />
+        </span>
+        <span> | Da ban 1000+</span>
+      </WrapperReportText>
+      <WrapperPriceText>
+        1.000.000đ
+        <span style={{ marginRight: "8px" }}></span>
+        <WrapperDiscountText>5%</WrapperDiscountText>
+      </WrapperPriceText>
+    </WrapperCardStyle>
+  );
+};
+
+export default CardComponent;
